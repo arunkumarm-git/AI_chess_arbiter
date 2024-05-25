5dbf266f65134e331f108e0a30fffb4099151cd4
arunmass-git <marunkumar1601@gmail.com>
2024-05-25 23:32:56 +0530

    added chess 2 gif link

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ def firstmessage():
 def other_tools():
     for i in range(6):  # For space
         st.write(" ")
-
+    st.write("Other useful links 👇")
     st.link_button("Vibe with chess", "https://vibewithchess.com")
     st.link_button("Chess review", "https://chessvibe.onrender.com/")
+    st.link_button("Chess 2 Gif", "https://chess2gif.streamlit.app/")
